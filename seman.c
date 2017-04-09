efce5a8feb1dc84899965e3be4f2535a59566404
Dalton <dalton.scharff@gmail.com>
2017-04-09 13:15:22 -0400

    a little work done to seman.c

--- seman.c
+++ seman.c
@@ -15,23 +15,6 @@ void traverse();
 void traverse_recursive(tree root);
 
 
-void traverse(){
-	traverse_recursive(root);
-}
-
-void traverse_recursive(tree root){
-	if(IsNull(root)){
-		printf("null");
-		return;
-	}
-}
-
-int main(int argc, char* argv){
-	treelst = stdout;
-	yyparse();
-} 
-
-
 /*	Implements loc_str function found in proj3.c	*/
 int loc_str(char search[]){
 	int i = 0;
@@ -49,3 +32,55 @@ int loc_str(char search[]){
 	}
 	return -1;
 }
+
+
+/*	Starts recursive call to traverse whole tree 	*/
+void traverse(){
+	traverse_recursive(root);
+}
+
+
+/*	Recursively iterates through whole tree 	*/
+void traverse_recursive(tree root){
+	if(IsNull(root)){
+		return;
+	}else{
+		traverse_recursive(LeftChild(root));
+		analyze(root);
+	}
+}
+
+
+/*	Calls different functions based on root node kind/op	*/
+void analyze(tree root){
+	if(NodeKind(root) == STRINGNode){
+
+	}else{
+		if(NodeOp(root) == ClassOp){
+
+		}else if(NodeOp(root) == BodyOp){
+
+		}else if(NodeOp(root) == DeclOp){
+			
+		}else if(NodeOp(root) == BoundOp){
+			
+		}else if(NodeOp(root) == CommaOp){
+			
+		}else if(NodeOp(root) == StmtOp){
+			
+		}else if(NodeOp(root) == RArgTypeOp || NodeOp(root) == VArgTypeOp){
+			
+		}
+	}
+}
+
+
+int main(int argc, char* argv){
+	treelst = stdout;
+	yyparse();
+
+	STInit();
+	traverse();
+	STPrint();
+	
+} 
